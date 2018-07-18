83a88e7c092676b0c46453765417507d05533272
Maxime Morariu-Patrichi <maximemorariu@me.com>
2018-07-18 11:01:48 +0100

    First release. Run this to compile the Cyton file.

---
+++ mpoints/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy
+
+# Run in the console (under Anaconda version): python setup.py build_ext --inplace
+
+# setup(
+#     ext_modules=cythonize("hybrid_hawkes_exp_likelihood.pyx"),
+#     include_dirs=[numpy.get_include()]
+# )
+
+ext_modules=[ Extension("hybrid_hawkes_exp_cython",
+              ["hybrid_hawkes_exp_cython.pyx"],
+              # libraries=["m"],  # comment this line when compiling on Windows
+              extra_compile_args = ["-ffast-math"])]
+
+setup(
+  name = "hybrid_hawkes_exp_cython",
+  cmdclass = {"build_ext": build_ext},
+  ext_modules = ext_modules,
+  include_dirs=[numpy.get_include()])
