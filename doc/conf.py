4af3f96a638de7cb3269f5ecb7a14520df6a2529
Maxime Morariu-Patrichi <maximemorariu@me.com>
2018-09-13 22:47:24 +0100

    added mocking for readthedocs

--- doc/conf.py
+++ doc/conf.py
@@ -16,6 +16,15 @@ import os
 import sys
 sys.path.insert(0, '/Users/maximemorariu/Dropbox/Documents/PyCharm/Packages/mpoints')
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
 
