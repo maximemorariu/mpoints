f78fe22109e6f2ba750165b1b95f9d1faed347d2
Maxime Morariu-Patrichi <maximemorariu@me.com>
2018-09-13 22:48:19 +0100

    added mocking for readthedocs

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas']
+MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas', 'scipy', 'hybrid_hakwes_exp_cython']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
