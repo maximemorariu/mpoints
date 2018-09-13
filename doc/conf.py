df6c69552cc2701ca3c43d2356a8f5da2b695dec
Maxime Morariu-Patrichi <maximemorariu@me.com>
2018-09-13 23:18:19 +0100

    changed list of mocked packages.

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas', 'scipy', 'mpoints.hybrid_hawkes_exp_cython']
+MOCK_MODULES = ['numpy', 'scipy', 'mpoints.hybrid_hawkes_exp_cython']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
